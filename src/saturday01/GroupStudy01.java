538a6568c95baddc3d1180e9a242535ff367c0ec
selenucar <58497897+selenucar@users.noreply.github.com>
2021-08-07 21:57:02 -0500

    Update GroupStudy01.java

--- src/saturday01/GroupStudy01.java
+++ src/saturday01/GroupStudy01.java
@@ -4,5 +4,8 @@ public class GroupStudy01 {
 
     public static void main(String[] args) {
         System.out.println("BigFighters");
+        System.out.println("BigFighters");
+        System.out.println("BigFighters");
+        System.out.println("BigFighters");
     }
 }
